bd587d1aba720b42fcd16b280ca25ed3cf7f5766
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2022-10-12 17:14:15 +0300

    section async storage

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
+import { initWorkouts } from "../storage/workout";
 
 export default function useCachedResources() {
     const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -7,6 +8,7 @@ export default function useCachedResources() {
     useEffect(() => {
         async function loadResourcesAndDataAsync() {
             try {
+                await initWorkouts();
                 await Font.loadAsync({
                     "montserrat": require("../assets/fonts/Montserrat-Regular.ttf"),
                     "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
